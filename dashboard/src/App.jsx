c4117471af5a77d8b80e3d2bd27f1f812e0fed99
abhay734 <vermaabhay734@gmail.com>
2025-04-08 12:38:28 +0530

    Login and Register

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
+import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
+
 function App() {
-  return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  );
+    const [allRoutes, setAllRoutes] = useState([...publicRoutes])
+    console.log(allRoutes)
+    return <Router allRoutes={allRoutes} />  
 }
 
 export default App;
