9991bcf7aa069067ab8ead7536eff28209913b71
abhay734 <vermaabhay734@gmail.com>
2025-04-09 01:25:01 +0530

    Login and Register Page

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,6 @@
-import React,{lazy} from 'react';
+import React,{lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client'; 
- 
+import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,7 +10,9 @@ const App = lazy(() => import('./App'))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Suspense>
     <App />
+  </Suspense>
   </BrowserRouter>
 );
 
