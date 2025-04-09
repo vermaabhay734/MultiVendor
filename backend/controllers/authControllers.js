cf37a98ec022c26709444768b4579d0eafe68c0a
abhay734 <vermaabhay734@gmail.com>
2025-04-10 05:13:22 +0530

    Connect MongoDB with project

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,11 @@
 class authControllers{
     admin_login = async(req,res) => {
-        console.log(req.body)
+        const {email,password} = req.body
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const { dbConnect } = require('./utiles/db')
 
 app.use(cors({
     origin : ['http://localhost:3000'],
@@ -18,4 +19,5 @@ app.use('/api',require('./routes/authRoutes'))
 
 app.get('/',(req,res) => res.send('My backend'))
 const port = process.env.PORT
+dbConnect()
 app.listen(port, () => console.log(`Server is running on port ${port}`))
