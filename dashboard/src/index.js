e332fcf93016daf87d2d2ef80e10fb64b44fda8c
abhay734 <vermaabhay734@gmail.com>
2025-04-09 14:51:22 +0530

    Redux Toolkit Setup

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -4,15 +4,29 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-const App = lazy(() => import('./App'))
+import { Provider } from 'react-redux'; 
+import store from './store/index';
+import { Toaster } from 'react-hot-toast';
 
+const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Provider store={store}>
   <Suspense>
     <App />
+    <Toaster 
+      toastOptions={{
+        position : 'top-right',
+        style : {
+          background : '#283046',
+          color : 'white'
+        }
+      }}
+    />
   </Suspense>
+  </Provider>
   </BrowserRouter>
 );
 
