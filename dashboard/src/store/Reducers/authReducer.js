5ae23cd0ba3d6c3d70b4a7bf9df577ddaa46352f
abhay734 <vermaabhay734@gmail.com>
2025-04-11 04:41:22 +0530

    Success Notification

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -38,6 +38,10 @@ export const authReducer = createSlice({
             state.loader = false;
             state.errorMessage = payload.error 
         })
+        .addCase(admin_login.fulfilled, (state, { payload }) => {
+            state.loader = false;
+            state.successMessage = payload.message
+        })
     }
 })
 
--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { admin_login, messageClear  } from '../../store/Reducers/authReducer';
 import { PropagateLoader } from 'react-spinners';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 
 const AdminLogin = () => {
 
+    const navigate = useNavigate()
     const dispatch = useDispatch()
-    const {loader, errorMessage} = useSelector(state=>state.auth)
+    const {loader, errorMessage, successMessage} = useSelector(state=>state.auth)
 
     const [state, setState] = useState({
         email: "",
@@ -41,7 +43,12 @@ const AdminLogin = () => {
             toast.error(errorMessage)
             dispatch(messageClear())
         }
-    },[errorMessage])
+        if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear())  
+            navigate('/')          
+        }
+    },[errorMessage,successMessage])
  
 
     return (
