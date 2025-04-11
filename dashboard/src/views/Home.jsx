466b12e9d3a45ee15029c3b341060e14ac451f31
abhay734 <vermaabhay734@gmail.com>
2025-04-11 16:19:58 +0530

    Manage Routes and Middleware

--- dashboard/src/views/Home.jsx
+++ dashboard/src/views/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Sidebar = () => {
+const Home = () => {
     return (
         <div>
-            <h1>Sidebar</h1>
+            <h1>Home Page</h1>
         </div>
     );
 };
 
-export default Sidebar;
+export default Home;
