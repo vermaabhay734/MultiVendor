8a001c16aa40504481151b74c7f640c92f7988fd
abhay734 <vermaabhay734@gmail.com>
2025-04-14 17:55:08 +0530

    Header Profile and Search Functionality

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -8,6 +8,23 @@ const Header = ({showSidebar, setShowSidebar}) => {
                 <div onClick={() => setShowSidebar(!showSidebar)} className='w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer ' >
                     <span><FaList/></span>
                 </div>
+
+                <div className='hidden md:block'>
+                    <input className='px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#423d72] focus:border-indigo-300 overflow-hidden' type="text" name='search' placeholder='search' />
+                </div>
+
+                <div className='flex justify-center items-center gap-8 relative'>
+                    <div className='flex justify-center items-center'>
+                        <div className='flex justify-center items-center gap-3'>
+                            <div className='flex justify-center items-center flex-col text-end'>
+                                <h2 className='text-md font-bold'>Abhay Verma</h2>
+                                <span className='text-[14px] w-full font-normal'>Admin</span>
+                            </div>
+                            <img className='w-[45px] h-[45px] rounded-full overflow-hidden' src="http://localhost:3000/images/admin.jpg" alt="" />
+                        </div>
+                        
+                    </div>
+                </div>
             </div> 
         </div>
     );
