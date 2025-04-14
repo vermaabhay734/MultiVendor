2739f1ded4ff8b8fc5e9f0ef920bf741d78b2518
abhay734 <vermaabhay734@gmail.com>
2025-04-14 17:11:11 +0530

    Navigation Sidebar created

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,16 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { getNav } from '../navigation/index';
+import { BiLogOutCircle } from "react-icons/bi";
+
 
 const Sidebar = () => {
 
+    const {pathname} = useLocation()
 
     const [allNav,setAllNav] = useState([])
     useEffect(() => {
         const navs = getNav('admin')
         setAllNav(navs)
     },[])
-    console.log(allNav)
+    // console.log(allNav)
 
 
     return (
@@ -23,7 +26,22 @@ const Sidebar = () => {
                     </Link>
                 </div>
                 <div className='px-[16px]'>
-
+                    <ul>
+                        {
+                            allNav.map((n,i) =><li key={i}>
+                                <Link to={n.path} className={`${pathname === n.path ? 'bg-blue-600 shadow-indigo-500/50 text-white duration-500' : 'text-[#030811] font-bold duration-200 ' } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 `} >
+                                    <span>{n.icon}</span>
+                                    <span>{n.title}</span>
+                                </Link>
+                            </li> )
+                        }
+                        <li>
+                            <button className='text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                            <span><BiLogOutCircle /></span>
+                            <span>Logout</span>
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
