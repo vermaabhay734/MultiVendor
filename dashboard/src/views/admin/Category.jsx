d95e71e5d72eeb708111be5f9d1759313deae75a
abhay734 <vermaabhay734@gmail.com>
2025-04-17 02:34:09 +0530

    Category Page Design Part 3

--- dashboard/src/views/admin/Category.jsx
+++ dashboard/src/views/admin/Category.jsx
@@ -6,7 +6,10 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 
 const Category = () => {
 
+    const [currentPage, setCurrentPage] = useState(1)
+    const [searchValue, setSearchValue] = useState('')
     const [parPage, setParPage] = useState(5)
+    const [show, setShow] =  useState(false)
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
@@ -53,12 +56,29 @@ const Category = () => {
                                 </tbody>
                             </table>
                         </div>
+                        <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
+                            <Pagination 
+                                pageNumber = {currentPage}
+                                setPageNumber = {setCurrentPage}
+                                totalItem = {50}
+                                parPage = {parPage}
+                                showItem = {3}
+                            />
+                        </div>
                     </div>
                 </div>
 
-                <div className='w-full lg:w-7/12'>
-                    <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
-
+                <div className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed ${show ? 'right-0' : '-right-[340px]'} z-20 top-0 transition-all duration-500`}>
+                    <div className='w-full pl-5'>
+                        <div className='bg-[#6a5fdf] h-screen lg:h-auto px-3 py-2 lg:rounded-md text-[#d0d2d6]'>
+                            <h1 className='text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center '>Add Category</h1>
+                            <form>
+                                <div className='flex flex-col w-full gap-1 mb-3'>
+                                    <label htmlFor="name"> Category Name</label>
+                                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#ffffff] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" id='name' name='category_name' placeholder='Category Name' />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
