f092597f1cb6e3bbe0a8200083d8dd2214d43c3a
abhay734 <vermaabhay734@gmail.com>
2025-04-20 17:48:10 +0530

    Seller Chat Page Design Part 2

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -13,7 +13,43 @@ const ChatSeller = () => {
                         <div className='w-full h-[calc(100vh-177px)] bg-[#9e97e9] md:bg-transparent overflow-y-auto'>
                             <div className='flex text-xl justify-between items-center p-4 md:p-0 md:px-3 md:pb-3 text-white'>
                                 <h2>Sellers</h2>
-                                <span className='block cursor-pointer md:hidden'><IoMdClose /> </span>
+                                <span onClick={() => setShow(true)} className='block cursor-pointer md:hidden'><IoMdClose /> </span>
+                            </div>
+
+                            <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}>
+                                <div className="relative">
+                                    <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
+                                    <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
+                                </div>
+                                <div className='flex justify-center items-start flex-col w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <h2 className='text-base font-semibold'>Abhay Verma</h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}>
+                                <div className="relative">
+                                    <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
+                                    <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
+                                </div>
+                                <div className='flex justify-center items-start flex-col w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <h2 className='text-base font-semibold'>Raj Verma</h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}>
+                                <div className="relative">
+                                    <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
+                                    <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
+                                </div>
+                                <div className='flex justify-center items-start flex-col w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <h2 className='text-base font-semibold'>Ankit Verma</h2>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
