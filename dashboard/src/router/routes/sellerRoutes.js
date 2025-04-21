7e8cec077db3b56f98822f82fdbed8576925d4fb
abhay734 <vermaabhay734@gmail.com>
2025-04-22 05:16:59 +0530

    Add Product Page Design Part 1

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 const Home = lazy(()=> import('../../views/Home'))  
-const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))  
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
+const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))
 
 export const sellerRoutes = [
     {
@@ -12,6 +13,11 @@ export const sellerRoutes = [
         path: '/seller/dashboard',
         element : <SellerDashboard/>,
         ability : ['admin','seller']
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element : <AddProduct/>,
+        ability : ['seller']
     }
 
 ]
