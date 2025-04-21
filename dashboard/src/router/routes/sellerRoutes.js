5e5aaa039d2a321c469c36a03abdb7800e3141ec
abhay734 <vermaabhay734@gmail.com>
2025-04-22 02:35:56 +0530

    Seller Dashboard Page Design Part 1

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -1,10 +1,16 @@
-import { lazy } from "react";  
-const Home = lazy(()=> import('../../views/Home'))   
+import { lazy } from "react";
+const Home = lazy(()=> import('../../views/Home'))  
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))  
 
 export const sellerRoutes = [
     {
         path: '/',
         element : <Home/>,
+        ability : ['admin','seller']
+    },
+    {
+        path: '/seller/dashboard',
+        element : <SellerDashboard/>,
         ability : ['admin','seller']
     }
 
---
+++ dashboard/src/views/seller/SellerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellerDashboard = () => {
+    return (
+        <div>
+            <h2>Seller DAshboad</h2>
+        </div>
+    );
+};
+
+export default SellerDashboard;
