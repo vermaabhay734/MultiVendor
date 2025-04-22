00886556b9ae10ab9c1cf114632e1a19a817b672
abhay734 <vermaabhay734@gmail.com>
2025-04-23 04:50:07 +0530

    Product Image Add Manage Part 1

--- dashboard/src/views/seller/AddProduct.jsx
+++ dashboard/src/views/seller/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IoMdImages } from "react-icons/io";
 
 const AddProduct = () => {
     const categorys = [
@@ -61,8 +62,21 @@ const AddProduct = () => {
         } else {
             setAllCategory(categorys)
         }
-
     }
+
+    // For Images 
+    const [images, setImages] = useState([])
+    const [imageShow, setImageShow] = useState([])
+
+    const imageHandle = (e) => {
+        const files = e.target.files 
+        const length = files.length;
+        if (length > 0) {
+            setImages([...images, ...files])
+        }
+    }
+    console.log(images)
+
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
@@ -74,17 +88,22 @@ const AddProduct = () => {
                 <div>
                     <form>
                         <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
+
+                            {/* Product Name Section */}
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor="name">Product Name</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.name} type="text" name='name' id='name' placeholder='Product Name' />
                             </div>
 
+                            {/* Product Brand Section */}
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor="brand">Product Brand</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.brand} type="text" name='brand' id='brand' placeholder='Brand Name' />
                             </div>
                         </div>
                         <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
+
+                            {/* Category section */}
                             <div className='flex flex-col w-full gap-1 relative'>
                                 <label htmlFor="category">Category</label>
                                 <input readOnly onClick={()=> setCateShow(!cateShow)} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={category} type="text" id='category' placeholder='--Select category--' />
@@ -105,6 +124,8 @@ const AddProduct = () => {
                                     </div>
                                 </div>
                             </div>
+
+                            {/* Product Stock Section */}
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor="stock">Product Stock</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.stock} type="text" name='stock' id='stock' placeholder='Stock' />
@@ -112,20 +133,34 @@ const AddProduct = () => {
                         </div>
 
                         <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
+                            {/* Price Section */}
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor="price">Price</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.price} type="number" name='price' id='price' placeholder='price' />
                             </div>
 
+                            {/* Discount Section */}
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor="discount">Discount</label>
                                 <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.discount} type="number" name='discount' id='discount' placeholder='Discount by %' />
                             </div>
                         </div>
-                        <div className='flex flex-col w-full gap-1'>
+
+                        {/* Description Section */}
+                        <div className='flex flex-col w-full gap-1 mb-5'>
                             <label htmlFor="description" className='text-[#d0d2d6]'>Description</label>
                             <textarea className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.description} name='description' id='description' placeholder='Description' cols="10" rows="4"></textarea> 
-                        </div> 
+                        </div>
+                        
+                        {/* For Image section */}
+                        <div className='grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-3 w-full text-[#d0d2d6] mb-4'>
+                            <label className='flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-[#d0d2d6]' htmlFor="image">
+                                <span><IoMdImages /></span>
+                                <span>Select Image </span>
+                            </label>
+                            <input className='hidden' onChange={imageHandle} multiple type="file" id='image' />
+                        </div>
+
                     </form>
                 </div>
             </div>
