6ea9512ad2c09f3e547e93f76294802a1f4180c2
abhay734 <vermaabhay734@gmail.com>
2025-04-23 17:40:03 +0530

    Seller Orders Page Design

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -4,6 +4,7 @@ const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
 const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))
 const Products = lazy(()=> import('../../views/seller/Products'))
 const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
+const Orders = lazy(()=> import('../../views/seller/Orders'))
 
 export const sellerRoutes = [
     {
@@ -29,6 +30,11 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/discount-product',
         element : <DiscountProducts/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/orders',
+        element : <Orders/>,
         ability : ['seller']
     }
 
