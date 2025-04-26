91f7a4a4618fb5d8dfa45af7dfa7aa9c2dd69459
abhay734 <vermaabhay734@gmail.com>
2025-04-27 01:25:35 +0530

    Seller Register and Login Part 4

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,10 +1,12 @@
 const adminModel = require('../models/adminModel')
 const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
 const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
 
 class authControllers{
+    // For Admin Login
     admin_login = async(req,res) => {
         const {email,password} = req.body
         try {
@@ -34,27 +36,38 @@ class authControllers{
     }
     // End Method
 
-
+    
+    // For New Seller Registration
     seller_register = async(req, res) => {
-         const {email,name,password} = req.body
-         try {
-            const getUser = await sellerModel.findOne({email})
-            if (getUser) {
-                responseReturn(res,404,{error: 'Email Already Exit'})
-            }else{
-                const seller = await sellerModel.create({
-                    name,
-                    email,
-                    password: await bcrpty.hash(password, 10),
-                    method : 'menualy',
-                    shopInfo: {}
-                })
-                console.log(seller)
-            }
-         } catch (error) {
-            console.log(error)
-         }
-    }
+        const {email,name,password} = req.body
+        try {
+           const getUser = await sellerModel.findOne({email})
+           if (getUser) {
+               responseReturn(res,404,{error: 'Email Already Exit'})
+           }else{
+               const seller = await sellerModel.create({
+                   name,
+                   email,
+                   password: await bcrpty.hash(password, 10),
+                   method : 'menualy',
+                   shopInfo: {}
+               })
+              await sellerCustomerModel.create({
+                    myId: seller.id
+              })
+
+              const token = await createToken({ id : seller.id, role: seller.role })
+              res.cookie('accessToken',token, {
+               expires : new Date(Date.now() + 7*24*60*60*1000 )
+              })
+
+              responseReturn(res,201,{token,message: 'Register Success'})
+           }
+        } catch (error) {
+           responseReturn(res,500,{error: 'Internal Server Error'})
+        }
+   }
+   // End Method 
 
 
     getUser = async (req, res) => {
