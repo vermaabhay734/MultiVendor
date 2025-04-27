c7cce6206b3c33f44bdc4092904ccc7edeb2834a
abhay734 <vermaabhay734@gmail.com>
2025-04-28 00:40:03 +0530

    Seller Register and Login Part 7

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -36,6 +36,37 @@ class authControllers{
     }
     // End Method
 
+
+    // For seller login
+    seller_login = async(req,res) => {
+        const {email,password} = req.body
+        try {
+            const seller = await sellerModel.findOne({email}).select('+password')
+            // console.log(admin)
+            if (seller) {
+                const match = await bcrpty.compare(password, seller.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : seller.id,
+                        role : seller.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    })
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+            } else {
+                responseReturn(res,404,{error: "Email not Found"})
+            }
+        } catch (error) {
+            responseReturn(res,500,{error: error.message})
+        }
+    }
+    // End Method
+
     
     // For New Seller Registration
     seller_register = async(req, res) => {
