85990c646dfd4220e7b1f2bef37f9c4d191c4172
abhay734 <vermaabhay734@gmail.com>
2025-04-28 16:05:16 +0530

    Protect Route Manage Part 1

--- dashboard/src/router/routes/publicRoutes.js
+++ dashboard/src/router/routes/publicRoutes.js
@@ -3,6 +3,7 @@ const Home = lazy(()=> import('../../views/Home'))
 const Login = lazy(()=> import('../../views/auth/Login'))   
 const Register = lazy(()=> import('../../views/auth/Register')) 
 const AdminLogin = lazy(()=> import('../../views/auth/AdminLogin'))
+const UnAuthorized = lazy(()=> import('../../views/UnAuthorized'))
 
 const publicRoutes = [
     {
@@ -20,6 +21,10 @@ const publicRoutes = [
     {
         path : '/admin/login',
         element : <AdminLogin/>
+    },
+    {
+        path : '/unauthorized',
+        element : <UnAuthorized/>
     }
 ]
 
