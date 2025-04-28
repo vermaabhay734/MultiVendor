530149b9e1f8d3774d84ade78bca6ad354637af7
abhay734 <vermaabhay734@gmail.com>
2025-04-29 03:48:19 +0530

    Setup Add Category Via Admin Part 4

--- backend/routes/dashboard/categoryRoutes.js
+++ backend/routes/dashboard/categoryRoutes.js
@@ -2,6 +2,7 @@ const categoryController = require('../../controllers/dashboard/categoryControll
 const { authMiddleware } = require('../../middlewares/authMiddleware')
 const router = require('express').Router()
 
-router.post('/category-add',authMiddleware, categoryController.add_category) 
+router.post('/category-add',authMiddleware, categoryController.add_category)
+router.get('/category-get',authMiddleware, categoryController.get_category) 
 
 module.exports = router
