5bee61249fa65922725a1f2e76df61d270387515
abhay734 <vermaabhay734@gmail.com>
2025-05-12 04:54:08 +0530

    Feature Products Section Rating design in FrontEnd Part 3

--- frontend/src/components/products/FeatureProducts.jsx
+++ frontend/src/components/products/FeatureProducts.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FaEye, FaRegHeart } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
+import Rating from '../Rating';
 
 const FeatureProducts = () => {
     return (
         <div className='w-[85%] flex flex-wrap mx-auto'>
+            {/* Feature Products Heading. */}
             <div className='w-full'>
                 <div className='text-center flex justify-center items-center flex-col text-4xl text-slate-600 font-bold relative pb-[45px]'>
                     <h2>Feature Products </h2>
@@ -16,30 +18,35 @@ const FeatureProducts = () => {
                 {
                     [1,2,3,4,5,6].map((p,i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
                         <div className='relative overflow-hidden'>
-                        
                             <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>8% </div>
 
                             <img className='sm:w-full w-full h-[240px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
 
                             {/* Hover Icon in Image of feature products. */}
                             <ul className='flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3'>
+                                {/* Wishlist */}
                                 <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
                                 <FaRegHeart />
                                 </li>
+                                {/* Product Details */}
                                 <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
                                 <FaEye />
                                 </li>
+                                {/* Shopping Cart */}
                                 <li className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
                                 <RiShoppingCartLine />
                                 </li>
                             </ul>
                         </div>
 
-                        {/* Product Name and Price */}
+                        {/* Product Name, Price and Rating */}
                         <div className='py-3 text-slate-600 px-2'>
                             <h2 className='font-bold'>Product Name </h2>
                             <div className='flex justify-start items-center gap-3'>
                                 <span className='text-md font-semibold'>Rs.656</span>
+                                <div className='flex'>
+                                    <Rating ratings={4.5} />
+                                </div>
                             </div>
                         </div>
 
