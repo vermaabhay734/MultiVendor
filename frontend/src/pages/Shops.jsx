c15d36d5dc363168963143faf6571743c14240c4
abhay734 <vermaabhay734@gmail.com>
2025-05-14 00:41:45 +0530

    Setup Shop Page Design (Price Slider Filter) Part 3

--- frontend/src/pages/Shops.jsx
+++ frontend/src/pages/Shops.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from "react-icons/io";
+import { Range } from 'react-range';
 
 const Shops = () => {
 
@@ -18,12 +19,14 @@ const Shops = () => {
         'Smart Watches'
     ]
 
+    const [state, setState] = useState({values: [50, 1500]})
+
 
     return (
         <div>
             <Header/>
 
-            {/* banner page */}
+            {/* banner page section */}
             <section className='bg-[url("http://localhost:3000/images/banner/shop.png")] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left'>
                 <div className='absolute left-0 top-0 w-full h-full bg-[#2422228a]'>
                     <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
@@ -51,6 +54,7 @@ const Shops = () => {
 
                     <div className='w-full flex flex-wrap'>
                         <div className={`w-3/12 md-lg:w-4/12 md:w-full pr-8 ${filter ? 'md:h-0 md:overflow-hidden md:mb-6' : 'md:h-auto md:overflow-auto md:mb-0' } `}>
+                            {/* Category Section */}
                             <h2 className='text-3xl font-bold mb-3 text-slate-600'>Category </h2>
                             <div className='py-2'>
                                 {
@@ -60,6 +64,29 @@ const Shops = () => {
                                     </div>)
                                 }
                             </div>
+                            
+                            {/* Price slider filter */}
+                            <div className='py-2 flex flex-col gap-5'>
+                                <h2 className='text-3xl font-bold mb-3 text-slate-600'>Price</h2>
+                                <Range
+                                    step={5}
+                                    min={50}
+                                    max={1500}
+                                    values={(state.values)}
+                                    onChange={(values) => setState({values})}
+                                    renderTrack={({props,children}) => (
+                                        <div {...props} className='w-full h-[6px] bg-slate-200 rounded-full cursor-pointer'>
+                                            {children}
+                                        </div>
+                                    )}
+                                    renderThumb={({ props }) => (
+                                        <div className='w-[15px] h-[15px] bg-[#059473] rounded-full' {...props} />
+                                    )} 
+                                />
+                            </div>
+
+                            <span className='text-slate-800 font-bold text-lg'>${Math.floor(state.values[0])} - ${Math.floor(state.values[1])}</span>
+
                         </div>
                     </div>
                 </div>
