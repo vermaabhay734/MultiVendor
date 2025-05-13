c9f77e4b74c5d26188a277762f07b5047eb51cef
abhay734 <vermaabhay734@gmail.com>
2025-05-14 00:33:41 +0530

    Setup Shop Page Design (Category Section) Part 2

--- frontend/src/pages/Shops.jsx
+++ frontend/src/pages/Shops.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from "react-icons/io";
 
 const Shops = () => {
+
+    const [filter, setFilter] = useState(true)
+    const categorys = [
+        'Mobiles',
+        'Laptops',
+        'Speakers',
+        'Top wear',
+        'Footwear',
+        'Watches',
+        'Home Decor',
+        'Smart Watches'
+    ]
+
+
     return (
         <div>
             <Header/>
 
+            {/* banner page */}
             <section className='bg-[url("http://localhost:3000/images/banner/shop.png")] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left'>
                 <div className='absolute left-0 top-0 w-full h-full bg-[#2422228a]'>
                     <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
@@ -20,6 +35,30 @@ const Shops = () => {
                                 <IoIosArrowForward />
                                 </span>
                                 <span>Shop </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            {/* Category Section */}
+            <section className='py-16'>
+                <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
+                    <div className={` md:block hidden ${!filter ? 'mb-6' : 'mb-0'} `}>
+                        <button onClick={() => setFilter(!filter)} className='text-center w-full py-2 px-3 bg-indigo-500 text-white'>Filter Product</button>
+                    </div>
+
+                    <div className='w-full flex flex-wrap'>
+                        <div className={`w-3/12 md-lg:w-4/12 md:w-full pr-8 ${filter ? 'md:h-0 md:overflow-hidden md:mb-6' : 'md:h-auto md:overflow-auto md:mb-0' } `}>
+                            <h2 className='text-3xl font-bold mb-3 text-slate-600'>Category </h2>
+                            <div className='py-2'>
+                                {
+                                    categorys.map((c,i) => <div className='flex justify-start items-center gap-2 py-1'>
+                                        <input type="checkbox" id={c} />
+                                        <label className='text-slate-600 block cursor-pointer' htmlFor={c}>{c}</label>
+                                    </div>)
+                                }
                             </div>
                         </div>
                     </div>
