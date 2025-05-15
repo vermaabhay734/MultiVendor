79004060a8758cb14fa487bad1d355040d557a47
abhay734 <vermaabhay734@gmail.com>
2025-05-15 13:41:17 +0530

    Card page Design Setup Part 3

--- frontend/src/pages/Card.jsx
+++ frontend/src/pages/Card.jsx
@@ -6,7 +6,6 @@ import { IoIosArrowForward } from "react-icons/io";
 
 const Card = () => {
 
-    // For Stock products section
     const card_products = [1,2]
     const outOfStockProduct = [1,2]
 
@@ -21,11 +20,11 @@ const Card = () => {
                         <div className='flex flex-col justify-center gap-1 items-center h-full w-full text-white'>
                             <h2 className='text-3xl font-bold'>Card Page </h2>
                             <div className='flex justify-center items-center gap-2 text-2xl w-full'>
-                                    <Link to='/'>Home</Link>
-                                    <span className='pt-1'>
-                                    <IoIosArrowForward />
-                                    </span>
-                                    <span>Card </span>
+                        <Link to='/'>Home</Link>
+                        <span className='pt-1'>
+                        <IoIosArrowForward />
+                        </span>
+                        <span>Card </span>
                             </div>
                         </div> 
                     </div> 
@@ -44,41 +43,94 @@ const Card = () => {
                                             <h2 className='text-md text-green-500 font-semibold'>Stock Products {card_products.length}</h2>
                                         </div>
                                         {
-                                            <div className='flex bg-white p-4 flex-col gap-2'>
+                                            card_products.map((p,i) => <div className='flex bg-white p-4 flex-col gap-2'>
                                                 <div className='flex justify-start items-center'>
                                                     <h2 className='text-md text-slate-600 font-bold'>Easy Shop</h2>
                                                 </div>
-                                                {/* Products details */}
+
                                                 {
-                                                    <div className='w-full flex flex-wrap'>
-                                                        <div className='flex sm:w-full gap-2 w-7/12'>
-                                                            <div className='flex gap-2 justify-start items-center'>
-                                                                <img className='w-[80px] h-[80px]' src="http://localhost:3000/images/products/3.webp" alt="" />
-                                                                <div className='pr-4 text-slate-600'>
-                                                                    <h2 className='text-md font-semibold'>Product Name </h2>
-                                                                    <span className='text-sm'>Brand: Jara</span>
-                                                                </div>
+                                                    [1,2].map((p,i) => <div className='w-full flex flex-wrap'>
+                                                    <div className='flex sm:w-full gap-2 w-7/12'>
+                                                        <div className='flex gap-2 justify-start items-center'>
+                                                            <img className='w-[80px] h-[80px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
+                                                            <div className='pr-4 text-slate-600'>
+                                                                <h2 className='text-md font-semibold'>Product Name </h2>
+                                                                <span className='text-sm'>Brand: Jara</span>
                                                             </div>
                                                         </div>
-
-                                                        <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
-                                                            <div className='pl-4 sm:pl-0'>
-                                                                <h2 className='text-lg text-orange-500'>Rs.240</h2>
-                                                                <p className='line-through'>Rs.300</p>
-                                                                <p>-15%</p>
-                                                            </div>
-                                                        </div>
-
                                                     </div>
+
+                                                    <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
+                                                        <div className='pl-4 sm:pl-0'>
+                                                            <h2 className='text-lg text-orange-500'>$240</h2>
+                                                            <p className='line-through'>$300</p>
+                                                            <p>-15%</p>
+                                                        </div>
+                                                        <div className='flex gap-2 flex-col'>
+                                                            <div className='flex bg-slate-200 h-[30px] justify-center items-center text-xl'>
+                                                                <div className='px-3 cursor-pointer'>-</div> 
+                                                                <div className='px-3'>2</div> 
+                                                                <div className='px-3 cursor-pointer'>+</div> 
+                                                            </div>
+                                                            <button className='px-5 py-[3px] bg-red-500 text-white'>Delete</button>
+                                                        </div>
+                                                    </div>
+                                                </div>)
+                                                }
+                                            </div>) 
                                                 }
 
-                                            </div>
-                                        }
+                            {/* Out of Stock Products */}
+                            {
+                                outOfStockProduct.length > 0 && <div className='flex flex-col gap-3'>
+                                    <div className='bg-white p-4'>
+                                <h2 className='text-md text-red-500 font-semibold'>Out of Stock {outOfStockProduct.length}</h2>
+                                        </div>
+
+                            <div className='bg-white p-4'>
+                            {
+                                [1].map((p,i) => <div className='w-full flex flex-wrap'>
+                                <div className='flex sm:w-full gap-2 w-7/12'>
+                                    <div className='flex gap-2 justify-start items-center'>
+                                <img className='w-[80px] h-[80px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
+                                        <div className='pr-4 text-slate-600'>
+                                        <h2 className='text-md font-semibold'>Product Name </h2>
+                                        <span className='text-sm'>Brand: Jara</span>
+                                        </div>
                                     </div>
                                 </div>
+
+                                <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
+                                    {/* Product discount */}
+                                    <div className='pl-4 sm:pl-0'>
+                                        <h2 className='text-lg text-orange-500'>$240</h2>
+                                        <p className='line-through'>$300</p>
+                                        <p>-15%</p>
+                                    </div>
+
+                                    {/* Quantity changed or removed */}
+                                    <div className='flex gap-2 flex-col'>
+                                        <div className='flex bg-slate-200 h-[30px] justify-center items-center text-xl'>
+                                            <div className='px-3 cursor-pointer'>-</div> 
+                                            <div className='px-3'>2</div> 
+                                            <div className='px-3 cursor-pointer'>+</div> 
+                                        </div>
+                                        <button className='px-5 py-[3px] bg-red-500 text-white'>Delete</button>
+                                    </div>
+                                </div>
+                            </div>)
+                            }
+                            </div>           
+
+                                </div>
+                            }
+                                    </div>
+
+                                </div>
                             </div>
-                        </div> 
-                        
+                        </div>
+
+                        // if cart is empty then only shop now button
                         : <div>
                             <Link className='px-4 py-1 bg-indigo-500 text-white' to='/shops' > Shop Now</Link>
                         </div>
