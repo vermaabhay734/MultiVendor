693eeb183c2b5deac03aadf6c3042336a6b51e29
abhay734 <vermaabhay734@gmail.com>
2025-05-15 23:11:31 +0530

    Card page Design Setup Part 4

--- frontend/src/pages/Card.jsx
+++ frontend/src/pages/Card.jsx
@@ -62,8 +62,8 @@ const Card = () => {
 
                                                     <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
                                                         <div className='pl-4 sm:pl-0'>
-                                                            <h2 className='text-lg text-orange-500'>$240</h2>
-                                                            <p className='line-through'>$300</p>
+                                                            <h2 className='text-lg text-orange-500'>Rs.240</h2>
+                                                            <p className='line-through'>Rs.300</p>
                                                             <p>-15%</p>
                                                         </div>
                                                         <div className='flex gap-2 flex-col'>
@@ -103,8 +103,8 @@ const Card = () => {
                                 <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
                                     {/* Product discount */}
                                     <div className='pl-4 sm:pl-0'>
-                                        <h2 className='text-lg text-orange-500'>$240</h2>
-                                        <p className='line-through'>$300</p>
+                                        <h2 className='text-lg text-orange-500'>Rs.240</h2>
+                                        <p className='line-through'>Rs.300</p>
                                         <p>-15%</p>
                                     </div>
 
@@ -125,7 +125,39 @@ const Card = () => {
                                 </div>
                             }
                                     </div>
+                                </div>
+                            </div>
 
+                            {/* Order Summary Section */}
+                            <div className='w-[33%] md-lg:w-full'>
+                                <div className='pl-3 md-lg:pl-0 md-lg:mt-5'>
+                                    {
+                                        card_products.length > 0 && <div className='bg-white p-3 text-slate-600 flex flex-col gap-3'>
+                                            <h2 className='text-xl font-bold'>Order Summary</h2>
+                                            <div className='flex justify-between items-center'>
+                                                <span>2 Items </span>
+                                                <span>Rs.343 </span>
+                                            </div>
+
+                                            <div className='flex justify-between items-center'>
+                                                <span>Shipping Fee </span>
+                                                <span>Rs.40 </span>
+                                            </div>
+                                            {/* Coupon Sec */}
+                                            <div className='flex gap-2'>
+                                                <input className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-green-500 rounded-sm' type="text" placeholder='Input Vauchar Coupon' />
+                                                <button className='px-5 py-[1px] bg-[#059473] text-white rounded-sm uppercase text-sm'>Apply</button>
+                                            </div>
+
+                                            <div className='flex justify-between items-center'>
+                                                <span>Total</span>
+                                                <span className='text-lg text-[#059473]'>Rs.430 </span>
+                                            </div>
+                                            <button className='px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg bg-red-500 text-sm text-white uppercase '>
+                                                Process to Checkout 
+                                            </button>
+                                        </div>
+                                    }
                                 </div>
                             </div>
                         </div>
