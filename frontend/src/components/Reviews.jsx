d395e5080c37365cebd3bc4e07f8f3209de530b1
abhay734 <vermaabhay734@gmail.com>
2025-05-17 18:08:41 +0530

    Product Reviews Page Design (Customer Rate & Feedback) Part 4

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -2,11 +2,23 @@ import React, {useState} from 'react';
 import Rating from './Rating';
 import RatingTemp from './RatingTemp';
 import Pagination from './Pagination';
+import { Link } from 'react-router-dom';
+import RatingReact from 'react-rating'
+import { FaStar } from 'react-icons/fa';
+import { CiStar } from 'react-icons/ci';
 
 const Reviews = () => {
 
+    // For Pagination
     const [parPage, setParPage] = useState(1)
     const [pageNumber, setPageNumber] = useState(10)
+
+    // Customer login or not
+    const userInfo = {}
+
+    // For set rating star
+    const [rat, setRat] = useState('')
+    const [re, setRe] = useState('')
 
 
     return (
@@ -26,6 +38,7 @@ const Reviews = () => {
 
                 {/* Rating stars and graph */}
                 <div className='flex gap-2 flex-col py-4'>
+                    {/* If rating is 5 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={5} />
@@ -37,6 +50,7 @@ const Reviews = () => {
                         <p className='text-sm text-slate-600 w-[0%]'>10</p>
                     </div>
 
+                    {/* If rating is 4 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={4} />
@@ -48,6 +62,7 @@ const Reviews = () => {
                         <p className='text-sm text-slate-600 w-[0%]'>20</p>
                     </div>
 
+                    {/* If rating is 3 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={3} />
@@ -59,6 +74,7 @@ const Reviews = () => {
                         <p className='text-sm text-slate-600 w-[0%]'>8</p>
                     </div>
 
+                    {/* If rating is 2 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={2} />
@@ -70,6 +86,7 @@ const Reviews = () => {
                         <p className='text-sm text-slate-600 w-[0%]'>5</p>
                     </div>
 
+                    {/* If rating is 1 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={1} />
@@ -81,6 +98,7 @@ const Reviews = () => {
                         <p className='text-sm text-slate-600 w-[0%]'>3</p>
                     </div>
 
+                    {/* If rating is 0 */}
                     <div className='flex justify-start items-center gap-5'>
                         <div className='text-md flex gap-1 w-[93px]'>
                             <RatingTemp rating={0} />
@@ -117,6 +135,36 @@ const Reviews = () => {
                         <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}  totalItem={10} parPage={parPage} showItem={Math.floor(10 / 3)} />
                     }
                 </div>
+            </div>
+
+            {/* Customer Feedback Comment only if user is login */}
+            <div>
+                {
+                userInfo ? <div className='flex flex-col gap-3'>
+
+                    {/* Star Rating */}
+                    <div className='flex gap-1'>
+                        <RatingReact
+                        onChange={(e) => setRat(e)}
+                        initialRating={rat}
+                        emptySymbol={<span className='text-slate-600 text-4xl'><CiStar/></span>}
+                        fullSymbol={<span className='text-[#Edbb0E] text-4xl'><FaStar/></span>}
+                        />
+                    </div>
+
+                    {/* Comment TextArea */}
+                    <form>
+                        <textarea required className='border outline-0 p-3 w-full' name="" id="" cols="30" rows="5"></textarea>
+                        <div className='mt-2'>
+                            <button className='py-1 px-5 bg-indigo-500 text-white rounded-sm'>Submit</button>
+                        </div>
+                    </form>
+
+                    {/* If user is not login then */}
+                    </div> : <div>
+                        <Link to='/login' className='py-1 px-5 bg-red-500 text-white rounded-sm'> Login First </Link>
+                    </div>
+                }
             </div>
         </div>
     );
