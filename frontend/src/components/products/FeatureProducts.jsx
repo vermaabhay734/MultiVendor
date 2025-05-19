9ff6f1377790e4d1234021881b446ca949d21361
abhay734 <vermaabhay734@gmail.com>
2025-05-19 15:57:11 +0530

    Fetch Feature Products and Latest Top Rated discount Dynamically Part 3

--- frontend/src/components/products/FeatureProducts.jsx
+++ frontend/src/components/products/FeatureProducts.jsx
@@ -4,7 +4,7 @@ import { RiShoppingCartLine } from "react-icons/ri";
 import Rating from '../Rating';
 import { Link } from 'react-router-dom';
 
-const FeatureProducts = () => {
+const FeatureProducts = ({products}) => {
     return (
         <div className='w-[85%] flex flex-wrap mx-auto'>
             {/* Feature Products Heading. */}
@@ -17,11 +17,13 @@ const FeatureProducts = () => {
             
             <div className='w-full grid grid-cols-4 md-lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
                 {
-                    [1,2,3,4,5,6].map((p,i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
+                    products.map((p,i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
                         <div className='relative overflow-hidden'>
-                            <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>8% </div>
+                            {
+                                p.discount ? <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{p.discount}% </div> : ''
+                            }
 
-                            <img className='sm:w-full w-full h-[240px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
+                            <img className='sm:w-full w-full h-[240px]' src={p.images[0]} alt="" />
 
                             {/* Hover Icon in Image of feature products. */}
                             <ul className='flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3'>
@@ -42,11 +44,11 @@ const FeatureProducts = () => {
 
                         {/* Product Name, Price and Rating */}
                         <div className='py-3 text-slate-600 px-2'>
-                            <h2 className='font-bold'>Product Name </h2>
+                            <h2 className='font-bold'>{p.name}</h2>
                             <div className='flex justify-start items-center gap-3'>
-                                <span className='text-md font-semibold'>Rs.656</span>
+                                <span className='text-md font-semibold'>Rs.{p.price}</span>
                                 <div className='flex'>
-                                    <Rating ratings={4.5} />
+                                    <Rating ratings={p.rating} />
                                 </div>
                             </div>
                         </div>
