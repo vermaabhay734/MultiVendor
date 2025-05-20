9d3d83e0d0267fb75e2988fcaf824b469903b512
abhay734 <vermaabhay734@gmail.com>
2025-05-21 01:23:47 +0530

    Customer Registration Setup Part 2

---
+++ backend/controllers/home/customerAuthController.js
@@ -0,0 +1,8 @@
+class customerAuthController{
+
+    customer_register = async(req,res) => {
+        console.log(req.body)
+    }
+}
+
+module.exports = new customerAuthController()
--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,6 +1,22 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
 
+export const customer_register = createAsyncThunk(
+    'auth/customer_register',
+    async(info, { rejectWithValue,fulfillWithValue }) => {
+        try {
+            const {data} = await api.post('/customer/customer-register',info)
+            localStorage.setItem('customerToken',data.token)
+            // console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.respone)
+        }
+    }
+)
+// End Method
+
+
 export const authReducer = createSlice({
     name: 'auth',
     initialState:{
@@ -15,11 +31,13 @@ export const authReducer = createSlice({
             state.errorMessage = ""
             state.successMessage = ""
         }
- 
     },
     extraReducers: (builder) => {
         
     }
 })
+// End Method
+
+
 export const {messageClear} = authReducer.actions
 export default authReducer.reducer
