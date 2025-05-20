1a1e0a696d0d32f4c1cc4916f9708a3d6972564f
abhay734 <vermaabhay734@gmail.com>
2025-05-21 01:29:30 +0530

    Customer Registration Setup Part 3

--- backend/controllers/home/customerAuthController.js
+++ backend/controllers/home/customerAuthController.js
@@ -1,8 +1,29 @@
 class customerAuthController{
 
     customer_register = async(req,res) => {
-        console.log(req.body)
+        const {name, email, password } = req.body
+
+        try {
+            const customer = await customerModel.findOne({email}) 
+            if (customer) {
+                responseReturn(res, 404,{error : 'Email Already Exits'} )
+            } else {
+                const createCustomer = await customerModel.create({
+                    name: name.trim(),
+                    email: email.trim(),
+                    password: await bcrypt.hash(password, 10),
+                    method: 'menualy'
+                })
+                await sellerCustomerModel.create({
+                    myId: createCustomer.id
+                })
+                
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
     }
+    // End Method
 }
 
 module.exports = new customerAuthController()
