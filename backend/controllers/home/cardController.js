7bf3e0968efeb3e1c539bcd96b55ae056c952f1a
abhay734 <vermaabhay734@gmail.com>
2025-05-22 18:45:48 +0530

    Product Add to Card Part 3

--- backend/controllers/home/cardController.js
+++ backend/controllers/home/cardController.js
@@ -1,7 +1,40 @@
+const cardModel = require('../../models/cardModel')
+const { responseReturn } = require('../../utiles/response')
+
 class cardController{
    
     add_to_card =  async(req, res) => {
-        console.log(req.body)
+        const { userId, productId, quantity } = req.body
+        // It will check whether user is login or lot then only add to card works otherwise redirect to login page.
+        try {
+            const product = await cardModel.findOne({
+                $and: [{
+                    productId : {
+                        $eq: productId
+                    }
+                },
+                {
+                    userId: {
+                        $eq: userId
+                    }
+                }
+            ]
+            })
+
+            if (product) {
+                responseReturn(res,404,{error: "Product Already Added To Card" })
+            } else {
+                const product = await cardModel.create({
+                    userId,
+                    productId,
+                    quantity
+                })
+                responseReturn(res,201,{message: "Added To Card Successfully" , product})
+            }
+            
+        } catch (error) {
+            console.log(error.message)
+        }
     }
     // End Method 
 }
