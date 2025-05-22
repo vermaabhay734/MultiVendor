66e034643a97238945c4aded78c9fa15f09b8e9c
abhay734 <vermaabhay734@gmail.com>
2025-05-22 19:16:04 +0530

    Cart Count Icon Dynamically update in Header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,6 +18,7 @@ const Header = () => {
     const navigate = useNavigate()
     const {categorys} = useSelector(state => state.home)
     const {userInfo} = useSelector(state => state.auth)
+    const {card_product_count} = useSelector(state => state.card) 
 
     const {pathname} = useLocation()
 
@@ -31,6 +32,15 @@ const Header = () => {
 
     const search = () => {
         navigate(`/products/search?category=${category}&&value=${searchValue}`)
+    }
+
+    // Card count icon
+    const redirect_card_page = () => {
+        if (userInfo) {
+            navigate('/card')
+        } else {
+            navigate('/login')
+        }
     }
 
 
@@ -144,13 +154,15 @@ const Header = () => {
                                         </div>
 
                                         {/* Shopping Cart Icon */}
-                                        <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+                                        <div onClick={redirect_card_page} className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
                                             <span className='text-xl text-green-500'><FaCartShopping  /></span>
-                                            <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
-                                                {
-                                                    wishlist_count
-                                                }
-                                            </div>
+                                            {
+                                                card_product_count !== 0 && <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
+                                                    {
+                                                        card_product_count
+                                                    }
+                                                </div>
+                                            }
                                         </div>
                                     </div>
                                 </div>
