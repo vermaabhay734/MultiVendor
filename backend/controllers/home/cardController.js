fcdb792e5973e1c158097595c0540e36eb9a89d3
abhay734 <vermaabhay734@gmail.com>
2025-05-24 09:08:09 +0530

    Cart Product Get and Show Part 4

--- backend/controllers/home/cardController.js
+++ backend/controllers/home/cardController.js
@@ -41,20 +41,50 @@ class cardController{
 
 
     get_card_products = async(req, res) => {
-       const {userId } = req.params
-       try {
-        const card_products = await cardModel.aggregate([{
-            $match: {
-                userId: {
-                    $eq: new ObjectId(userId)
+
+        const {userId } = req.params
+        try {    
+            const card_products = await cardModel.aggregate([{
+                $match: {
+                    userId: {
+                        $eq: new ObjectId(userId)
+                    }
                 }
+            },
+            {
+                $lookup : {
+                    from: 'products',
+                    localField: 'productId',
+                    foreignField: "_id",
+                    as: 'products'
+                }
+            } 
+            ])
+            let buy_product_item = 0
+            let calculatePrice = 0;
+            let card_product_count = 0;
+            const outOfStockProduct = card_products.filter(p => p.products[0].stock < p.quantity)
+            for (let i = 0; i < outOfStockProduct.length; i++) {
+                card_product_count = card_product_count + outOfStockProduct[i].quantity        
             }
-        }])
-            console.log(card_products)
-       } catch (error) {
-         console.log(error.message)
-       }
-       
+            const stockProduct = card_products.filter(p => p.products[0].stock >= p.quantity)
+            for (let i = 0; i < stockProduct.length; i++) {
+                const { quantity } = stockProduct[i]
+                card_product_count = buy_product_item + quantity
+
+                buy_product_item = buy_product_item + quantity
+                const { price,discount } = stockProduct[i].products[0]
+                if (discount !== 0) {
+                    calculatePrice = calculatePrice + quantity * (price - Math.floor((price * discount) / 100 ))
+                } else {
+                    calculatePrice  = calculatePrice + quantity * price
+                }        
+            } // end for
+            let p = []
+                console.log(calculatePrice)
+            } catch (error) {
+            console.log(error.message)
+        }
     }
     // End Method
 
